85f2a1e43761f4855e8a3a91a6bf6243c2b44ef1
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-08-30 13:35:23 +0530

    add

--- Anjali Maam/loops/print number partten.js
+++ Anjali Maam/loops/print number partten.js
@@ -1,12 +1,78 @@
-function printPartten(row) {
+// function printPartten(row) {
+//     let str;
+//     for (let i = 1; i <= row; i++) {
+//         str = "";
+//         for (let j = 1; j <= row; j++) {
+//             str += i;
+//         }
+//         console.log(str);
+//     }
+// }
+// printPartten(5);
+
+//================================
+// function printPartten(row) {
+//     let str;
+//     for (let i = 1; i <= row; i++) {
+//         str = "";
+//         for (let j = 1; j <= row; j++) {
+//             str += j;
+//         }
+//         console.log(str);
+//     }
+// }
+// printPartten(5);
+
+//================================================
+// function printPartten(row) {
+//     let str;
+//     for (let i = 1; i <= row; i++) {
+//         str = "";
+//         for (let j = 1; j <= row ; j++) {
+//             str += " ";
+//         }
+//         for (let k = 0; k != 2 * i - 1; k++) {
+//             str += i;
+//         }
+//         console.log(str);
+//     }
+// }
+// printPartten(5);
+
+//=====================================================
+// function printPartten(row) {
+//     let str;
+//     for (let i = 1; i <= row; i++) {
+//         str = "";
+//         for (let j = 1; j <= row -1; j++) {
+//             str += " ";
+//         }
+//         for (let k = 0; k != 2 * i - 1; k++) {
+//             str += i;
+//         }
+//         console.log(str);
+//     }
+// }
+// printPartten(5);
+
+//===================================================
+function printPartten(row, val, col) {
     let str;
+
     for (let i = 1; i <= row; i++) {
-        str = " ";
+        str = "";
+        val = i;
+        col = 2 * i - 1;
         for (let j = 1; j <= row - i; j++) {
             str += " ";
         }
-        for (let k = 0; k != 2 * i - 1; k++) {
-            str += "*";
+        for (let k = 0; k != col; k++) {
+            str += val;
+            if (k < col / 2 - 1) {
+                val--;
+            } else {
+                val++;
+            }
         }
         console.log(str);
     }
